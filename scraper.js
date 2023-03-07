a1755a2788fb5a693374f99621917b7c71c9ca0a
Marge <marge.consunji@gmail.com>
2023-03-07 15:46:02 +0800

    Navigate to Company Announcements and get news
    - Set search function to limit results to dividend announcements
    - Set fixed date for now

--- scraper.js
+++ scraper.js
@@ -14,6 +14,18 @@ async function scrape() {
   // Failure to set this leads to timeout when loading PSE Edge
   await page.setDefaultNavigationTimeout(0);
   await page.goto(process.env.PSE_NEWS);
+
+  try {
+    await page.$eval('input[id="tmplNm"]', templateInput => templateInput.value = "Declaration of Cash Dividends");
+    // set date limits
+    await page.$eval('input[name="fromDate"]', fromDate => fromDate.value = '03-01-2023');
+    await page.$eval('input[name="toDate"]', toDate => toDate.value = '03-01-2023');
+    await page.$eval('input[id="btnSearch"]', searchElem => searchElem.click());
+    await page.waitForNavigation();
+    await page.$$eval()
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 // Run function
