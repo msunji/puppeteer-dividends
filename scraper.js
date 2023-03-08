987d33801a0d8d7a9f335c51cf21e4660d07f960
Marge <marge.consunji@gmail.com>
2023-03-08 17:04:49 +0800

    Check if announcements were made
    Return 'no announcements today' if there aren't any announcements for the day, otherwise, return arr of cells.

--- scraper.js
+++ scraper.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const puppeteer = require('puppeteer');
 
-const announcements = [];
+const announcements = {};
 const announcement = {
   'companyName': '',
   'templateName': '',
@@ -13,7 +13,7 @@ const announcement = {
 
 // Puppeteer code starts here
 async function scrape() {
-  // Init browser - we'll do headless: false for now
+  // Init browser
   const browser = await puppeteer.launch({
     headless: true,
     defaultViewport: null
@@ -42,11 +42,32 @@ async function scrape() {
     const announcementData = await page.$$eval('tbody tr', rows => {
       return rows.map(row => {
         const cells = row.querySelectorAll('td'); // recall that querySelectorAll returns a nodelist NOT an array
-        return Array.from(cells, cell => cell.innerHTML);
+
+        // If no announcements are made, there should only be one cell in one row
+        if (cells.length === 1) {
+          return 'No announcements today.';
+        } else {
+          return Array.from(cells, cell => cell.innerHTML);
+        }
       });
     })
-    console.log('table', announcementData);
-    // await browser.close();
+
+    console.log(announcementData);
+
+    // Check if any announcements were made
+    // if (announcementData[0].length === 1) {
+    //   console.log('No announcements today');
+    //   return;
+    //   // TO DO: this needs to be closed properly
+    // } else {
+    //   // Parse announcement data
+    //   // console.log('table', announcementData);
+
+    // }
+
+    // Parse announcementData
+    // console.log(announcementData[0][0].replace(/<\s*[^>]*>/gi, ''));
+    await browser.close();
   } catch (err) {
     console.error(err);
     await browser.close();
