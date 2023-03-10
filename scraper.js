9fb14b27109710b5e19dc996bc451827b77d52c5
Marge <marge.consunji@gmail.com>
2023-03-10 19:07:42 +0800

    Get relevenant news info and clean up code a bit
    Really need to clean things up (keep things DRY and all), but for now, this seems to work well. Able to get company name, announcement date, payment amount and date, etc.

--- scraper.js
+++ scraper.js
@@ -39,32 +39,56 @@ async function scrape() {
       });
     })
 
-    console.log(announcementLinks);
+    // console.log(announcementLinks);
 
     const announcementData = [];
 
     if (announcementLinks.length) {
       for (const link of announcementLinks) {
         await page.goto(link, { waitUntil: 'domcontentloaded' });
-        await page.waitForSelector('#viewHeader');
-        await page.waitForSelector('#viewBody');
+        await page.waitForSelector('#viewHeader, #viewBody, iframe[id="viewContents"]');
 
+        // Get company name and announcement date from news item specific page on viewer
         const company = await page.$eval('#viewHeader h2', elem => elem.innerText);
         const announcementDate = await page.$eval('#viewHeader p', elem => elem.innerText.split(': ')[1]);
 
+        const detailsFrame = await page.$('iframe[id="viewContents"]');
+        const detailsContent = await detailsFrame.contentFrame();
+
+        // Table containing dividend information doesn't have an id specified, so we'll use Xpath
+
+        // const testFunc = () => {
+        //   return console.log('hello test');
+        // }
+        // Type of Dividend
+        const [typeTr] = await detailsContent.$x('//th[contains(text(),"Type (Regular or Special)")]/following-sibling::td[1]/descendant::span');
+        const typeVal = await typeTr.evaluate(el => el.textContent);
+
+        // Amount of Cash per Dividend per Share
+        const [amountTr] = await detailsContent.$x('//th[contains(text(),"Amount of Cash Dividend Per Share")]/following-sibling::td[1]/descendant::span');
+        const amountVal = await amountTr.evaluate(el => el.textContent);
+
+        // Payment Date
+        const [paymentTr] = await detailsContent.$x('//th[contains(text(),"Payment Date")]/following-sibling::td[1]/descendant::span');
+        const paymentVal = await paymentTr.evaluate(el => el.textContent);
+
+
+        // testFunc();
         const announcement = {
           'company': company,
-          'announcementDate':  announcementDate
+          'announcementDate':  announcementDate,
+          'type': typeVal,
+          'amount': amountVal,
+          'payment': paymentVal
         }
-
         announcementData.push(announcement);
       }
     } else {
-      return console.log('Nothing new to announce')
+      return console.log('Nothing new to announce');
     }
 
     console.log(announcementData);
-    // await browser.close();
+    await browser.close();
   } catch (err) {
     console.error(err);
     await browser.close();
