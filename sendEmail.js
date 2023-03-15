8495ffd2469acb1e682c61219e5c952700b66730
Marge <marge.consunji@gmail.com>
2023-03-15 12:26:44 +0800

    Fix nodemailer sendMail call
    Formatting errors were causing it to not return a console log with the messageId. This has been sorted.

--- sendEmail.js
+++ sendEmail.js
@@ -7,7 +7,7 @@ const path = require('path');
 // Get email template from templates directory
 const emailTemplate = fs.readFileSync(path.join(__dirname, '/templates/email.handlebars'), 'utf-8');
 
-function sendEmail(dataObj) {
+function sendEmail(announcementArr) {
   // Set up transporter object
   // Using Sendgrid for this one
   let transporter = nodemailer.createTransport({
@@ -19,29 +19,34 @@ function sendEmail(dataObj) {
     }
   });
 
-  // Us the Handlebars template we made earlier
-  const template = handlebars.compile(emailTemplate);
-  // Pass the data to the handlebars template
-  const msgBody = (template({ "data": dataObj }));
+  // Only send email if announcementArr isn't empty
+  // ie. there IS something to report
+  if (announcementArr.length) {
+    // Use the Handlebars template we made earlier
+    const template = handlebars.compile(emailTemplate);
+    // Pass the data to the handlebars template
+    const msgBody = (template({ "data": announcementArr }));
 
-  // Get list of recipients
-  const recipientList = process.env.RECIPIENT_EMAILS.split(",");
+    // Get list of recipients
+    const recipientList = process.env.RECIPIENT_EMAILS.split(",");
 
-  // Send mail!
-  transporter.sendMail({
-    from: `Marge Consunji <${process.env.SENDER_EMAIL}>`,
-    to: recipientList,
-    // to: 'mae.sunji@gmail.com',
-    subject: "Dividend Updates",
-    text: "These are today's updates",
-    html: msgBody
-  }), function(err, info) {
-    if (err) {
-      console.error(err)
-    } else {
-      console.log('Email sent: ' + info.response)
-    }
+    // Send mail!
+    transporter.sendMail({
+      from: `Marge Consunji <${process.env.SENDER_EMAIL}>`,
+      // to: recipientList,
+      to: 'mae.sunji@gmail.com',
+      subject: "PSE Dividend Updates",
+      text: "Good afternoon. Here are today's PSE Dividend Announcements.",
+      html: msgBody
+    }, (err, info) => {
+      if (err) {
+        return console.error(err)
+      }
+      return console.log('Email sent: ' + info.messageId);
+    })
   }
+
+
 }
 
 module.exports = sendEmail;
