0bbd169ea308a399a53ee6ad21d2a72423612e78
Marge <marge.consunji@gmail.com>
2023-03-15 13:02:42 +0800

    Refactor if-else block

--- scraper.js
+++ scraper.js
@@ -14,9 +14,8 @@ async function scraper() {
   let dateTodayArr = new Date().toISOString().slice(0,10).split('-');
   let parsedDateToday = [...dateTodayArr.slice(1), dateTodayArr[0]].join('-');
 
-  let announcementData = [];
-
   try {
+    let announcementData = [];
     const page = await browser.newPage();
 
     // Abort requests for images on PSE Edge
@@ -33,6 +32,7 @@ async function scraper() {
     // Go to search results for dividend announcements given set time
     // Pass the parsed date today as MM-DD-YYYY
     await page.setDefaultNavigationTimeout(0);
+    // await page.goto(process.env.TEST_NEWS, { waitUntil: 'domcontentloaded'});
     await page.goto(`${process.env.PSE_NEWS}&fromDate=${parsedDateToday}&toDate=${parsedDateToday}`, { waitUntil: 'domcontentloaded' });
 
     // Wait for tbody element to load
@@ -50,50 +50,49 @@ async function scraper() {
       });
     })
 
-    if (announcementLinks.length) {
-      for (const link of announcementLinks) {
-        await page.goto(link, { waitUntil: 'domcontentloaded' });
-        await page.waitForSelector('#viewHeader, #viewBody, iframe[id="viewContents"]');
-
-        // Get company name and announcement date from news item specific page on viewer
-        const company = await page.$eval('#viewHeader h2', elem => elem.innerText);
-        const announcementDate = await page.$eval('#viewHeader p', elem => elem.innerText.split(': ')[1]);
-
-        // Target elements in iframe
-        const detailsFrame = await page.$('iframe[id="viewContents"]');
-        const detailsContent = await detailsFrame.contentFrame();
-
-        // Get ex-date
-        const exDate = await detailsContent.$eval('#remarkContents span', elem => elem.innerText.split(': ')[1]);
-
-        // Table containing dividend information doesn't have an id specified, so we'll use Xpath
-        const getCellValue = async (cellLabel) => {
-          const [selectedCell] = await detailsContent.$x(`//th[contains(text(),"${cellLabel}")]/following-sibling::td[1]`);
-          const value = await selectedCell.evaluate(el => el.textContent);
-          return value;
-        };
-
-        // Get values for:
-        // Dividend Type, Amount of Cash div per share, record date and payment datae
-        const typeVal = await getCellValue('Type (Regular or Special)');
-        const amount = await getCellValue('Amount of Cash Dividend Per Share');
-        const recordDate = await getCellValue('Record Date');
-        const paymentDate = await getCellValue('Payment Date');
-
-        const announcement = {
-          company,
-          announcementDate,
-          typeVal,
-          amount,
-          exDate,
-          recordDate,
-          paymentDate
-        }
-        announcementData.push(announcement);
-      }
-    } else {
-      console.log('Nothing new to report');
+    if (!announcementLinks.length) {
+      console.log('Nothing new to announce');
       return announcementData;
+    }
+    for (const link of announcementLinks) {
+      await page.goto(link, { waitUntil: 'domcontentloaded' });
+      await page.waitForSelector('#viewHeader, #viewBody, iframe[id="viewContents"]');
+
+      // Get company name and announcement date from news item specific page on viewer
+      const company = await page.$eval('#viewHeader h2', elem => elem.innerText);
+      const announcementDate = await page.$eval('#viewHeader p', elem => elem.innerText.split(': ')[1]);
+
+      // Target elements in iframe
+      const detailsFrame = await page.$('iframe[id="viewContents"]');
+      const detailsContent = await detailsFrame.contentFrame();
+
+      // Get ex-date
+      const exDate = await detailsContent.$eval('#remarkContents span', elem => elem.innerText.split(': ')[1]);
+
+      // Table containing dividend information doesn't have an id specified, so we'll use Xpath
+      const getCellValue = async (cellLabel) => {
+        const [selectedCell] = await detailsContent.$x(`//th[contains(text(),"${cellLabel}")]/following-sibling::td[1]`);
+        const value = await selectedCell.evaluate(el => el.textContent);
+        return value;
+      };
+
+      // Get values for:
+      // Dividend Type, Amount of Cash div per share, record date and payment datae
+      const typeVal = await getCellValue('Type (Regular or Special)');
+      const amount = await getCellValue('Amount of Cash Dividend Per Share');
+      const recordDate = await getCellValue('Record Date');
+      const paymentDate = await getCellValue('Payment Date');
+
+      const announcement = {
+        company,
+        announcementDate,
+        typeVal,
+        amount,
+        exDate,
+        recordDate,
+        paymentDate
+      }
+      announcementData.push(announcement);
     }
     // console.log(announcementData);
     return announcementData;
