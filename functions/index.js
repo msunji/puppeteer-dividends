afd74032c714f7878495940428ea1e0a9f41a7dc
Marge <marge.consunji@gmail.com>
2023-03-16 16:43:24 +0800

    Try scheduled function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,8 @@
 const functions = require("firebase-functions");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.runCronJob = functions
+    .pubsub.schedule("35 16 * * 1-5")
+    .timeZone("Asia/Taipei")
+    .onRun(() => {
+      return console.log("This runs at 16.35 Monday to Friday");
+    });
