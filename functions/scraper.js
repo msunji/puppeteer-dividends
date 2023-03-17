f150f0ea6e3866c7b1c82afc7713eff7a2d123f8
Marge <marge.consunji@gmail.com>
2023-03-17 15:28:57 +0800

    Renamed and moved files, updated imports
    Running into issues importing scraper and sendemail modules from outside the functions directory. This should sort it out.

---
+++ functions/scraper.js
@@ -0,0 +1,113 @@
+/* eslint-disable max-len */
+require("dotenv").config();
+const puppeteer = require("puppeteer");
+
+// Puppeteer code starts here
+// eslint-disable-next-line require-jsdoc
+async function scraper() {
+  // Init browser
+  const browser = await puppeteer.launch({
+    headless: true,
+    defaultViewport: null,
+  });
+  // Get today's date in this format: MM-DD-YYYY
+  // This gets passed as a param to the URL we pass to Puppeteer
+  const dateTodayArr = new Date().toISOString().slice(0, 10).split("-");
+  const parsedDateToday = [...dateTodayArr.slice(1), dateTodayArr[0]].join("-");
+
+  try {
+    const announcementData = [];
+    const page = await browser.newPage();
+
+    // Abort requests for images on PSE Edge
+    // We don't need images, so best not to wait for them to load
+    await page.setRequestInterception(true);
+    page.on("request", (req) => {
+      if (req.resourceType() === "image") {
+        req.abort();
+      } else {
+        req.continue();
+      }
+    });
+
+    // Go to search results for dividend announcements given set time
+    // Pass the parsed date today as MM-DD-YYYY
+    await page.setDefaultNavigationTimeout(0);
+    // await page.goto(process.env.TEST_NEWS, { waitUntil: 'domcontentloaded'});
+    // eslint-disable-next-line max-len
+    await page.goto(`${process.env.PSE_NEWS}&fromDate=${parsedDateToday}&toDate=${parsedDateToday}`, {waitUntil: "domcontentloaded"});
+
+    // Wait for tbody element to load
+    await page.waitForSelector("tbody");
+
+    // Iterate through each table row and go through each td cell
+    const announcementLinks = await page.$$eval("tbody tr", (rows) => {
+      // Filter rows (must have more than one cell)
+      // and then map through cells and get viewer links per row
+      // Filter rows that only have one cell
+      // -> days with no cash dividend announcements
+      return rows.filter((row) => {
+        return row.querySelectorAll("td").length > 1;
+      }).map((row) => {
+        const cells = row.querySelectorAll("td");
+        return `https://edge.pse.com.ph/openDiscViewer.do?edge_no=${cells[1].childNodes[0].getAttribute("onclick").split("'")[1]}`;
+      });
+    });
+
+    if (!announcementLinks.length) {
+      console.log("Nothing new to announce");
+      return announcementData;
+    }
+    for (const link of announcementLinks) {
+      await page.goto(link, {waitUntil: "domcontentloaded"});
+      // eslint-disable-next-line max-len
+      await page.waitForSelector("#viewHeader, #viewBody, iframe[id=\"viewContents\"]");
+
+      // Get company name and announcement date
+      // from news item specific page on viewer
+      const company = await page.$eval("#viewHeader h2", (elem) => elem.innerText);
+      const announcementDate = await page.$eval("#viewHeader p", (elem) => elem.innerText.split(": ")[1]);
+
+      // Target elements in iframe
+      const detailsFrame = await page.$("iframe[id=\"viewContents\"]");
+      const detailsContent = await detailsFrame.contentFrame();
+
+      // Get ex-date
+      const exDate = await detailsContent.$eval("#remarkContents span", (elem) => elem.innerText.split(": ")[1]);
+
+      // Table containing dividend information
+      // doesn't have an id specified, so we'll use Xpath
+      const getCellValue = async (cellLabel) => {
+        const [selectedCell] = await detailsContent.$x(`//th[contains(text(),"${cellLabel}")]/following-sibling::td[1]`);
+        const value = await selectedCell.evaluate((el) => el.textContent);
+        return value;
+      };
+
+      // Get values for:
+      // Dividend Type, Amount of Cash div per share, record date and payment datae
+      const typeVal = await getCellValue("Type (Regular or Special)");
+      const amount = await getCellValue("Amount of Cash Dividend Per Share");
+      const recordDate = await getCellValue("Record Date");
+      const paymentDate = await getCellValue("Payment Date");
+
+      const announcement = {
+        company,
+        announcementDate,
+        typeVal,
+        amount,
+        exDate,
+        recordDate,
+        paymentDate,
+      };
+      announcementData.push(announcement);
+    }
+    // console.log(announcementData);
+    return announcementData;
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await browser.close();
+  }
+}
+
+module.exports = scraper;
--- functions/sendemail.js
+++ functions/sendemail.js
@@ -1,22 +1,25 @@
-require('dotenv').config();
-const nodemailer = require('nodemailer');
-const handlebars = require('handlebars');
-const fs = require('fs');
-const path = require('path');
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+const handlebars = require("handlebars");
+const fs = require("fs");
+const path = require("path");
 
 // Get email template from templates directory
-const emailTemplate = fs.readFileSync(path.join(__dirname, '/templates/email.handlebars'), 'utf-8');
+const emailTemplate = fs.readFileSync(
+    path.join(__dirname, "../templates/email.handlebars"), "utf-8",
+);
 
+// eslint-disable-next-line require-jsdoc
 function sendemail(announcementArr) {
   // Set up transporter object
   // Using Sendgrid for this one
-  let transporter = nodemailer.createTransport({
-    host: 'smtp.sendgrid.net',
+  const transporter = nodemailer.createTransport({
+    host: "smtp.sendgrid.net",
     port: 587,
     auth: {
       user: "apikey",
-      pass: process.env.SENDGRID_API
-    }
+      pass: process.env.SENDGRID_API,
+    },
   });
 
   // Only send email if announcementArr isn't empty
@@ -25,25 +28,25 @@ function sendemail(announcementArr) {
     // Use the Handlebars template we made earlier
     const template = handlebars.compile(emailTemplate);
     // Pass the data to the handlebars template
-    const msgBody = (template({ "data": announcementArr }));
+    const msgBody = (template({"data": announcementArr}));
 
     // Get list of recipients
-    const recipientList = process.env.RECIPIENT_EMAILS.split(",");
+
 
     // Send mail!
     transporter.sendMail({
       from: `Marge Consunji <${process.env.SENDER_EMAIL}>`,
       // to: recipientList,
-      to: 'mae.sunji@gmail.com',
+      to: "mae.sunji@gmail.com",
       subject: "PSE Dividend Updates",
       text: "Good afternoon. Here are today's PSE Dividend Announcements.",
-      html: msgBody
+      html: msgBody,
     }, (err, info) => {
       if (err) {
-        return console.error(err)
+        return console.error(err);
       }
-      return console.log('Email sent: ' + info.messageId);
-    })
+      return console.log("Email sent: " + info.messageId);
+    });
   }
 }
 
